2512f71152ef02383a4b16305ae24a8296879b1a
Anon Thamcharoen <anonnon3952@gmail.com>
2024-03-14 16:37:03 +0700

    datatable

--- src/app/modules/admin/products/products.component.ts
+++ src/app/modules/admin/products/products.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,19 +9,24 @@ import { ADTSettings } from 'angular-datatables/src/models/settings';
 import { FilePickerModule, ValidationError } from 'ngx-awesome-uploader';
 import { ProductComposeComponent } from './dialog/product-compose/product-compose.component';
 import { ProductService } from './product.service';
+import { ButtonNgComponent } from 'app/modules/datatables/button-ng/button-ng.component';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, DataTablesModule, MatButtonModule, MatIconModule, FilePickerModule],
+  imports: [CommonModule, DataTablesModule, MatButtonModule, MatIconModule, FilePickerModule, ButtonNgComponent],
   providers: [DatePipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, AfterViewInit {
   dtOptions: ADTSettings = {};
+  dtTrigger: Subject<ADTSettings> = new Subject<ADTSettings>();
 
   categories: any[] = [];
+
+  @ViewChild('btNg') btNg: TemplateRef<ButtonNgComponent>;
 
   constructor(
     public dialog: MatDialog,
@@ -32,42 +37,58 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
     this.productService.categories$.subscribe((resp) => this.categories = resp);
 
-    this.dtOptions = {
-      pageLength: 25,
-      serverSide: true,
-      ajax: (dataTablesParameters: any, callback) => {
-        this.productService.datatable(dataTablesParameters).subscribe({
-          next: (resp: any) => {
-            callback({
-              recordsTotal: resp.meta.totalItems,
-              recordsFiltered: resp.meta.totalItems,
-              data: resp.data
-            });
+    setTimeout(() => {
+      this.dtOptions = {
+        pageLength: 25,
+        serverSide: true,
+        ajax: (dataTablesParameters: any, callback) => {
+          this.productService.datatable(dataTablesParameters).subscribe({
+            next: (resp: any) => {
+              callback({
+                recordsTotal: resp.meta.totalItems,
+                recordsFiltered: resp.meta.totalItems,
+                data: resp.data
+              });
+            }
           }
-        }
-        )
-      },
-      columns: [{
-        title: 'เลขที่',
-        data: 'id'
-      }, {
-        title: 'รหัสสินค้า',
-        data: 'code'
-      }, {
-        title: 'สินค้า',
-        data: 'name'
-      }, {
-        title: 'วันที่และเวลา',
-        data: 'createdAt',
-        ngPipeInstance: this.datePipe,//เปลียนเวลาโดยการใช่ datepipe
-        ngPipeArgs: [" dd-MM-yyyy HH:mm น."]
-      },
-        //     {
-        //     title: 'ร้าน',
-        //     data: ' storeId'
-        // },
-      ]
-    };
+          )
+        },
+        columns: [
+          {
+            title: 'เลขที่',
+            data: 'id'
+          },
+          {
+            title: 'รหัสสินค้า',
+            data: 'code'
+          },
+          {
+            title: 'สินค้า',
+            data: 'name'
+          },
+          {
+            title: 'วันที่และเวลา',
+            data: 'createdAt',
+            ngPipeInstance: this.datePipe,//เปลียนเวลาโดยการใช่ datepipe
+            ngPipeArgs: [" dd-MM-yyyy HH:mm น."]
+          },
+          {
+            title: 'จัดการ',
+            data: null,
+            defaultContent: '',
+            ngTemplateRef: {
+              ref: this.btNg,
+            }
+          }
+        ]
+      };
+    });
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.dtTrigger.next(this.dtOptions);
+    }, 200);
   }
 
   opendialogapro() {
@@ -75,5 +96,10 @@ export class ProductsComponent implements OnInit {
       disableClose: true,
       data: {}
     });
+  }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
   }
 }
