c2f174924f629c86289d67eb34bf645de0f2aaab
View <mastertime7824@gmail.com>
2024-05-13 14:32:49 +0700

    dashboard

--- src/app/modules/admin/dashboard/dashboard.component.ts
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -16,7 +16,11 @@ import {
     ApexXAxis,
     ApexDataLabels,
     ApexTooltip,
-    ApexStroke
+    ApexStroke,
+    ApexPlotOptions,
+    ApexYAxis,
+    ApexLegend,
+    ApexGrid
 } from "ng-apexcharts";
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -24,6 +28,16 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { DashboardService } from './dashboard.service';
 
+type ApexXAxis1 = {
+    type?: "category" | "datetime" | "numeric";
+    categories?: any;
+    labels?: {
+      style?: {
+        colors?: string | string[];
+        fontSize?: string;
+      };
+    };
+  };
 
 export type ChartOptions = {
     series: ApexAxisChartSeries;
@@ -32,6 +46,11 @@ export type ChartOptions = {
     stroke: ApexStroke;
     tooltip: ApexTooltip;
     dataLabels: ApexDataLabels;
+    plotOptions: ApexPlotOptions;
+    yaxis: ApexYAxis;
+    grid: ApexGrid;
+    colors: string[];
+    legend: ApexLegend;
 };
 
 
@@ -46,6 +65,9 @@ export type ChartOptions = {
         NgFor, NgIf, MatTableModule, NgClass, CurrencyPipe, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
 })
 export class DashboardComponent {
+
+
+
     value1: string = '';
     value2: string = '';
     value3: string = '';
@@ -100,6 +122,8 @@ export class DashboardComponent {
 
     @ViewChild("chart") chart: ChartComponent;
     public chartOptions: Partial<ChartOptions>;
+    public chartOptions2: Partial<ChartOptions>;
+    public chartOptions3: Partial<ChartOptions>;
     selectedProject: string = 'ACME Corp. Backend App';
 
     constructor(private service: DashboardService) {
@@ -111,19 +135,162 @@ export class DashboardComponent {
         this.selectedFood = 'บางนา'; // ตั้งค่าเริ่มต้นให้เป็น บางนา หรือค่าใดก็ได้ตามที่ต้องการ
         this.onSelect(this.selectedFood);
 
+
+
+        this.chartOptions3 = {
+            series: [
+              {
+                name: "distibuted",
+                data: [21, 22]
+              }
+            ],
+            chart: {
+              height: 350,
+              type: "bar",
+              events: {
+                click: function(chart, w, e) {
+                  // console.log(chart, w, e)
+                }
+              }
+            },
+            colors: [
+              "#008FFB",
+              "#00E396",
+              "#FEB019",
+              "#FF4560",
+              "#775DD0",
+              "#546E7A",
+              "#26a69a",
+              "#D10CE8"
+            ],
+            plotOptions: {
+              bar: {
+                columnWidth: "45%",
+                distributed: true
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            legend: {
+              show: false
+            },
+            grid: {
+              show: false
+            },
+            xaxis: {
+              categories: [
+                ["เงินสด"],
+                ["พร้อมเพย์"],
+
+
+              ],
+              labels: {
+                style: {
+                  colors: [
+                    "#008FFB",
+                    "#00E396",
+                    "#FEB019",
+                    "#FF4560",
+                    "#775DD0",
+                    "#546E7A",
+                    "#26a69a",
+                    "#D10CE8"
+                  ],
+                  fontSize: "12px"
+                }
+              }
+            }
+          };
+
+
+        this.chartOptions2 = {
+            series: [
+              {
+                name: "distibuted",
+                data: [21, 22, 10, 28, 16]
+              }
+            ],
+            chart: {
+              height: 350,
+              type: "bar",
+              events: {
+                click: function(chart, w, e) {
+                  // console.log(chart, w, e)
+                }
+              }
+            },
+            colors: [
+              "#008FFB",
+              "#00E396",
+              "#FEB019",
+              "#FF4560",
+              "#775DD0",
+              "#546E7A",
+              "#26a69a",
+              "#D10CE8"
+            ],
+            plotOptions: {
+              bar: {
+                columnWidth: "45%",
+                distributed: true
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            legend: {
+              show: false
+            },
+            grid: {
+              show: false
+            },
+            xaxis: {
+              categories: [
+                ["ชานม"],
+                ["ชาไทย"],
+                ["ม่อคค่า"],
+                ["ช้อคโกแลตปั่น"],
+                ["นมหมีปั่น"],
+
+              ],
+              labels: {
+                style: {
+                  colors: [
+                    "#008FFB",
+                    "#00E396",
+                    "#FEB019",
+                    "#FF4560",
+                    "#775DD0",
+                    "#546E7A",
+                    "#26a69a",
+                    "#D10CE8"
+                  ],
+                  fontSize: "12px"
+                }
+              }
+            }
+          };
+
+
+
+
+
+
+
         this.chartOptions = {
             series: [
                 {
                     name: "สาขา1",
-                    data: [11, 31, 40, 28, 51, 42, 109, 100]
+                    data: [11, 31, 40, 28, 51, 42, 109, 100, 56, 43, 27, 67, 47, 27, 57, 83, 47, 26, 57, 97, 47, 58, 28]
                 },
                 {
                     name: "สาขา2",
-                    data: [11, 11, 32, 45, 32, 34, 52, 41]
+                    data: [11, 11, 32, 45, 32, 34, 52, 41, 48, 97, 35, 65, 87, 94, 79, 39, 79, 57, 35, 53, 79, 59, 29,]
                 },
                 {
                     name: "สาขา3",
-                    data: [11, 11, 38, 85, 32, 54, 22, 71]
+                    data: [11, 11, 38, 85, 32, 54, 22, 71, 75, 35, 68, 64, 79, 35, 57, 42, 58, 35, 79, 35, 24, 89, 100,]
                 }
 
             ],
@@ -146,7 +313,24 @@ export class DashboardComponent {
                     "2018-09-19T03:30:00.000Z",
                     "2018-09-19T04:30:00.000Z",
                     "2018-09-19T05:30:00.000Z",
-                    "2018-09-19T06:30:00.000Z"
+                    "2018-09-19T06:30:00.000Z",
+                    "2018-09-19T07:30:00.000Z",
+                    "2018-09-19T08:30:00.000Z",
+                    "2018-09-19T09:30:00.000Z",
+                    "2018-09-19T10:30:00.000Z",
+                    "2018-09-19T11:30:00.000Z",
+                    "2018-09-19T12:30:00.000Z",
+                    "2018-09-19T13:30:00.000Z",
+                    "2018-09-19T14:30:00.000Z",
+                    "2018-09-19T15:30:00.000Z",
+                    "2018-09-19T16:30:00.000Z",
+                    "2018-09-19T17:30:00.000Z",
+                    "2018-09-19T18:30:00.000Z",
+                    "2018-09-19T19:30:00.000Z",
+                    "2018-09-19T20:30:00.000Z",
+                    "2018-09-19T21:30:00.000Z",
+                    "2018-09-19T22:30:00.000Z",
+                    "2018-09-19T23:00:00.000Z",
                 ]
             },
             tooltip: {
@@ -155,6 +339,12 @@ export class DashboardComponent {
                 }
             }
         };
+
+
+
+
+
+
     }
     ngOnInit(): void {
         this.service.getBranchNames().subscribe(names => {
