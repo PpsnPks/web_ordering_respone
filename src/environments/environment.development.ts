7f70b9907cc0ba330479108b2fb0ea862c6af941
narongkorn-kevin <narongkorn.kevin@gmail.com>
2024-06-16 04:27:52 +0700

    update evn

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://upos-api.dev-asha.com'
-    apiUrl: 'https://sodexo-upos.ddns.net:3000'
+    apiUrl: 'https://upos-api.dev-asha.com'
+    // apiUrl: 'https://sodexo-upos.ddns.net:3000'
     // apiUrl: 'http://localhost:3000'
   };
