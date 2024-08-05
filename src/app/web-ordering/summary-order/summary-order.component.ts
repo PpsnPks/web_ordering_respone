763760f7d9f2031ec6284efb4238ab945550d748
PpsnPks <pipu8546@gmail.com>
2024-08-05 12:12:08 +0700

    summary-uncomplete

--- src/app/web-ordering/summary-order/summary-order.component.ts
+++ src/app/web-ordering/summary-order/summary-order.component.ts
@@ -7,11 +7,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { FormBuilder, FormControl, FormGroupDirective, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'asha-summary-order',
   standalone: true,
   imports: [
+    CommonModule,
     WebOrderingBarComponent,
     MatFormFieldModule,
     MatSelectModule,
@@ -26,6 +28,9 @@ export class SummaryOrderComponent {
   data: any
   nations: any
   form: FormGroup
+  orders: any
+  num_order: any = 0
+  sum_order: any = 4
 
   constructor(
     private _router: Router,
@@ -42,7 +47,22 @@ export class SummaryOrderComponent {
       nation: '',
       sex: ''
     })
+    this.orders = [
+      {name: 'กาแฟสังขยา (CUSTARD COFFEE)', order: 1, add: [{name:'เพิ่มช็อตกาแฟ 1', type: 'shot'}], price: 65, type:'normal'},
+      {name: 'Americano', order: 2, add: [{name: 'ขนาด M', type: 'size'}, {name: 'แถม โค้ก 1 ขวด', type: 'promotion'}], price: 85, type:'Pro'},
+      {name: 'COCOA', order: 1, add: [{name:'ขนาด S', type: 'size'}, {name:'ส่วนลด 2.00 บาท', type: 'discount'}], price: 60, type:'DC'},
+    ]
   }
 
+  changeNumOrder(signal: any, item: any){
+    if (signal == 'minus'){
+      item.order -= 1
+    }else if (signal == 'plus'){
+      item.order += 1
+    }
+  }
 
+  next(){
+    
+  }
 }
